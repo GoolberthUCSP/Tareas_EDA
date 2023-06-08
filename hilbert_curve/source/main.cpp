abc6cacd216078673a196bab1250b42c827e5ba3
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2023-06-08 18:41:11 -0500

    endeed hilbert curve

--- hilbert_curve/source/main.cpp
+++ hilbert_curve/source/main.cpp
@@ -21,10 +21,10 @@ FILE *out;
 void print_python_head(){
     fprintf(out,"import turtle\n");
     fprintf(out,"turtle.penup()\n");
-    //fprintf(out, "turtle.goto(-%d, %d)\n", H_WINDOWS/2 - STEP_SZ, W_WINDOWS/2 - STEP_SZ);
+    fprintf(out, "turtle.goto(-%d, %d)\n", H_WINDOWS/2 - STEP_SZ, W_WINDOWS/2 - STEP_SZ);
     fprintf(out,"turtle.pendown()\n");
     fprintf(out,"turtle.setup(%d, %d)\n", H_WINDOWS, W_WINDOWS);
-    //fprintf(out, "turtle.speed(%d)\n", SPEED);
+    fprintf(out, "turtle.speed(%d)\n", SPEED);
 	fprintf(out, "turtle.tracer(0)\n");
 }
 
@@ -34,76 +34,57 @@ void print_python_end(){
     fprintf(out, "screen.exitonclick()\n"); // EXIT ON CLICK
 }
 
-void hilbert_curve(int n, int angle) {
-    if( n==0 ) return;
-    fprintf(out,"turtle.right(%d)\n", angle);
-    
-    hilbert_curve(n-1, -angle);
-    
-    fprintf(out,"turtle.forward(%d)\n", STEP_SZ);
-    fprintf(out,"turtle.left(%d)\n", angle);
-    
-    hilbert_curve(n-1, angle);
-
-    fprintf(out,"turtle.forward(%d)\n", STEP_SZ);
-    
-    hilbert_curve(n-1, angle);
-
-    fprintf(out,"turtle.left(%d)\n", angle);
-    fprintf(out,"turtle.forward(%d)\n", STEP_SZ);
-
-    hilbert_curve(n-1, -angle);
-    
-    fprintf(out,"turtle.right(%d)\n", angle);
-}
-
-void hilbert_subprocess(int n, int angle, std::stringstream& buffer) {
+void hilbert_curve(int n, int angle, std::stringstream& buffer) {
     if (n == 0) return;
     buffer << "turtle.right(" << angle << ")\n";
 
-    hilbert_subprocess(n - 1, -angle, buffer);
+    hilbert_curve(n - 1, -angle, buffer);
 
     buffer << "turtle.forward(" << STEP_SZ << ")\n";
     buffer << "turtle.left(" << angle << ")\n";
 
-    hilbert_subprocess(n - 1, angle, buffer);
+    hilbert_curve(n - 1, angle, buffer);
 
     buffer << "turtle.forward(" << STEP_SZ << ")\n";
 
-    hilbert_subprocess(n - 1, angle, buffer);
+    hilbert_curve(n - 1, angle, buffer);
 
     buffer << "turtle.left(" << angle << ")\n";
     buffer << "turtle.forward(" << STEP_SZ << ")\n";
 
-    hilbert_subprocess(n - 1, -angle, buffer);
+    hilbert_curve(n - 1, -angle, buffer);
 
     buffer << "turtle.right(" << angle << ")\n";
 }
 
-std::string generate_hilbert_curve(int n) {
+void hilbert_curve_parallel(int n, int angle) {
     std::stringstream buffer;
-
+    std::stringstream buffers[4];
+    int angles[4] = {-angle, angle, angle, -angle};
+    buffers[0] << "turtle.right(" << angle << ")\n";
+    buffers[1] << "turtle.forward(" << STEP_SZ << ")\n";
+    buffers[1] << "turtle.left(" << angle << ")\n";
+    buffers[2] << "turtle.forward(" << STEP_SZ << ")\n";
+    buffers[3] << "turtle.left(" << angle << ")\n";
+    buffers[3] << "turtle.forward(" << STEP_SZ << ")\n";
     #pragma omp parallel for
     for (int i = 0; i < 4; ++i) {
-        std::stringstream local_buffer;
-        hilbert_subprocess(n - 1, (i % 2 == 0) ? 90 : -90, local_buffer);
-
+        hilbert_curve(n - 1, angles[i], buffers[i]);
         #pragma omp critical
-        buffer << local_buffer.rdbuf();
+        buffer << buffers[i].str();
     }
-
-    return buffer.str();
+    buffer << "turtle.right(" << angle << ")\n";
+    fprintf(out, "%s\n", buffer.str().c_str());
 }
 
 int main(int argc, char const *argv[]) {
     out = fopen("hilbert_turtle.py", "w+");
     print_python_head();
 
-    int order = 5;
+    int order = 7;
     int angle = 90;
-    
-    std::string buffer = generate_hilbert_curve(order);
-    fprintf(out, "%s\n", buffer.c_str());
+
+    hilbert_curve_parallel(order, angle);
 
     print_python_end();
     fclose(out);
